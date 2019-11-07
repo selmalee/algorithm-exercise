b62ef07420ff57af2fef3aed2fb86d583ac92f9b
seminelee <747191197@qq.com>
2019-11-08 00:41:47 +0800

    [*]. 7,9

--- easy/9.回文数.js
+++ easy/9.回文数.js
@@ -2,6 +2,7 @@
  * @lc app=leetcode.cn id=9 lang=javascript
  *
  * [9] 回文数
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
  */
 
 // @lc code=start
@@ -10,7 +11,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    
+  return Number(x.toString().split('').reverse().join('')) === x
 };
 // @lc code=end
 
