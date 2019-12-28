2f3ced744b600b7812edea9c255749b99b2a4076
seminelee <747191197@qq.com>
2019-12-29 01:31:06 +0800

    :art: easy/66

--- easy/66.加一.js
+++ easy/66.加一.js
@@ -16,11 +16,14 @@ var plusOne = function(digits) {
   const len = digits.length
   for (let i = len - 1; i > -1; i--) {
     if (digits[i] === 9) {
+      // 模拟进位
       digits[i] = 0
+      // 如果是最高位，那么要在前面补一
       if (i === 0) {
         digits.unshift(1)
       }
     } else {
+      // 直接加一
       digits[i] += 1
       break
     }
