9d146a8b500eb521ede1360eaa982302156ef911
seminelee <747191197@qq.com>
2020-01-21 15:57:27 +0800

    :sparkles:medium/15

--- medium/15.三数之和.js
+++ medium/15.三数之和.js
@@ -9,28 +9,79 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// 方案一：哈希表+暴力查找 时间复杂度O(n^2)
+// var threeSum = function(nums) {
+//   let hash = {}
+//   let hashMid = {}
+//   const len = nums.length
+//   let res = []
+//   nums = nums.sort()
+//   for (let i = 0; i < len; i++) {
+//     const num1 = nums[i]
+//     for (let j = i + 1; j < len; j++) {
+//       const num2 = nums[j]
+//       if (hash[num1 + num2] !== undefined) {
+//         const num0 = hash[num1 + num2]
+//         if (hashMid[num0] !== num1) {
+//           res.push([num0, num1, num2])
+//         }
+//         hashMid[num0] = num1
+//       }
+//     }
+//     hash[-num1] = num1
+//   }
+//   return res
+// };
+
+// 方案二：先排序，排序后遍历数字作为第一个数nums[i]，这时第二个数nums[s]肯定在nums[i]的右边，第三个数nums[t]肯定在nums[s]的右边，因此分别用两个指针s,t代替，分别指向i + 1 和 len - 1。
+// 每次循环判断nums[i] + nums[s] + nums[t] === 0，如果小于0则s++；如果大于0则t--；如果等于0保存起来，s++，t--。
+// 再考虑去重，即每次循环中nums[i] === nums[i - 1]的情况，以及nums[s] === nums[s + 1]和nums[t] === nums[t - 1]的情况。
 var threeSum = function(nums) {
-  let hash = {}
-  let hashMid = {}
-  const len = nums.length
   let res = []
-  nums = nums.sort()
-  console.log(nums)
+  // 鲁棒性
+  if (!nums || nums.length < 3) {
+    return res
+  }
+  const len = nums.length
+  // 升序排序
+  nums = nums.sort((a, b) => a - b)
+  // 遍历数组，nums[i]作为第一个数
   for (let i = 0; i < len; i++) {
-    const num1 = nums[i]
-    for (let j = i + 1; j < len; j++) {
-      const num2 = nums[j]
-      if (hash[num1 + num2] !== undefined) {
-        if (hashMid[num1] !== hash[num1 + num2]) {
-          res.push([hash[num1 + num2], num1, num2])
+    let s = i + 1 // 第二个数指针
+    let t = len - 1 // 第三个数指针
+    // 如果第一个数>0，则提前结束循环
+    if (nums[i] > 0) {
+      break
+    }
+    // 去重
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue
+    }
+    while(s < t) {
+      const num1 = nums[i] // 第一个数
+      const num2 = nums[s] // 第二个数
+      const num3 = nums[t] // 第三个数
+      const sum = num1 + num2 + num3 // 三数之和
+      if (sum === 0) {
+        // 等于0，目标数组
+        res.push([num1, num2, num3])
+        // 去重
+        while (nums[s] === nums[s + 1]) {
+          s++
         }
-        hashMid[num1] = hash[num1 + num2]
+        while (nums[t] === nums[t - 1]) {
+          t--
+        }
+        s++
+        t--
+      } else if (sum > 0) {
+        t--
+      } else {
+        s++
       }
     }
-    hash[-num1] = num1
   }
   return res
 };
-console.log(threeSum([-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]))
 // @lc code=end
 
