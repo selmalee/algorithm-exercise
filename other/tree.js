bbda658569b1c2646f3377bf22f00db725cdc6cc
seminelee <747191197@qq.com>
2020-04-08 17:29:14 +0800

    :sparkles: 广度优先遍历

--- other/tree.js
+++ other/tree.js
@@ -1,14 +1,35 @@
-// 中序遍历
-function midWalkTree(node) {
-  let array = []
-  if (node.left) {
-    array = array.concat(midWalkTree(node.left))
+// 深度优先遍历
+// 前序遍历 根-左-右
+// （中序：左-根-右；后序：左-右-根）
+var dfs = function (treeNode) {
+  let res = []
+  if (!treeNode) {
+    return res
   }
-  array.push(node.val)
-  if (node.right) {
-    array = array.concat(midWalkTree(node.right))
+  res.push(treeNode.val)
+  // 遍历左子树直到叶子节点
+  res = res.concat(dfs(treeNode.left))
+  // 遍历右子树直到叶子节点
+  res = res.concat(dfs(treeNode.right))
+  return res
+}
+
+// 广度优先遍历
+var bfs = function (treeNode) {
+  let queue = [treeNode] // 接下来要遍历的节点
+  let res = []
+  while(queue.length !== 0) {
+    var node = queue.shift() // 当前节点
+    res.push(node.val)
+    // 把左节点和右节点放到队列中等待遍历
+    if (node.left) {
+      queue.push(node.left)
+    }
+    if (node.right) {
+      queue.push(node.right)
+    }
   }
-  return array
+  return res
 }
 
 // function TreeNode(x) {
