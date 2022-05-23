765ad6b3a2aa09e8e58c5bd39ee4c507bd2389a9
seminelee <747191197@qq.com>
2022-05-23 09:41:53 +0800

    U 125

--- microsoft/125.验证回文串.js
+++ microsoft/125.验证回文串.js
@@ -8,9 +8,33 @@
 /**
  * @param {string} s
  * @return {boolean}
+ * 时间复杂度：O(|s|)，其中 |s| 是字符串 s 的长度。
+ * 空间复杂度：O(1)。
  */
 var isPalindrome = function(s) {
-
+  const len = s && s.length;
+  if (len < 2) {
+    return true;
+  }
+  let i = 0;
+  let j = len - 1;
+  while(i < j) {
+    if (!/[0-9a-zA-Z]/.test(s[i])) {
+      i++;
+      continue;
+    }
+    if (!/[0-9a-zA-Z]/.test(s[j])) {
+      j--;
+      continue;
+    }
+    if (s[i].toLowerCase() === s[j].toLowerCase()) {
+      i++;
+      j--;
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 // @lc code=end
 
